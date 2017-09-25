d6c2e3dc1e255fe39b690d94460028bba982ea04
Ming Hao <minghao2016@gmail.com>
2017-09-25 18:35:22 -0400

    upload

---
+++ dthybrid/demo_dthybrid.R
@@ -0,0 +1,130 @@
+
+
+setwd("C:\\Users\\Administrator\\Desktop\\chemogenomicAlg4DTIpred\\dthybrid")
+
+rm(list = ls())
+
+## current data set name
+db <- "nr"
+
+switch (db,
+        en = {
+          cat("en data\n")
+          flush.console()
+          sd <- read.table("e_simmat_dc.txt")
+          sd <- as.matrix(sd)
+          st <- read.table("e_simmat_dg.txt")
+          st <- as.matrix(st)
+          Y <- read.table("e_admat_dgc.txt")
+          Y <- as.matrix(Y) 
+          Y <- t(Y)         
+        },
+        ic = {
+          cat("ic data\n")
+          flush.console()
+          sd <- read.table("ic_simmat_dc.txt")
+          sd <- as.matrix(sd)
+          st <- read.table("ic_simmat_dg.txt")
+          st <- as.matrix(st)
+          Y <- read.table("ic_admat_dgc.txt")
+          Y <- as.matrix(Y)
+          Y <- t(Y)
+        },
+        gpcr = {
+          cat("gpcr data\n")
+          flush.console()
+          sd <- read.table("gpcr_simmat_dc.txt")
+          sd <- as.matrix(sd)
+          st <- read.table("gpcr_simmat_dg.txt")
+          st <- as.matrix(st)
+          Y <- read.table("gpcr_admat_dgc.txt")
+          Y <- as.matrix(Y)
+          Y <- t(Y)
+        },
+        nr = {
+          cat("nr data\n")
+          flush.console()
+          sd <- read.table("nr_simmat_dc.txt")
+          sd <- as.matrix(sd)
+          st <- read.table("nr_simmat_dg.txt")
+          st <- as.matrix(st)
+          Y <- read.table("nr_admat_dgc.txt")
+          Y <- as.matrix(Y)
+          Y <- t(Y)
+        },
+        stop("db should be one of the follows: 
+             {en, ic, gpcr, nr}\n")
+        )
+
+## load required packages
+pkgs <- c("matrixcalc", "data.table", "Rcpp", "ROCR", "Bolstad2", "MESS")
+rPkgs <- lapply(pkgs, require, character.only = TRUE)
+
+## source required R files
+rSourceNames <- c(
+  "doCVPositiveOnly.R",
+  "doCVPositiveOnly3.R",
+  "evalMetrics.R",
+  "Recommendation.R"
+)
+rSN <- lapply(rSourceNames, source, verbose = FALSE)
+
+
+Y <- t(Y)
+tmp <- sd
+sd <- st
+st <- tmp
+
+Y[1:3, 1:3]
+
+## do cross-validation
+kfold <- 10
+numSplit <- 5
+
+## DT-Hybrid method
+savedFolds <- doCVPositiveOnly3(Y, kfold = kfold, numSplit = numSplit)
+
+## saving results
+resMetrics <- matrix(NA, nrow = kfold, ncol = 1)
+colnames(resMetrics) <- c("MPR")
+resMetrics <- as.data.frame(resMetrics)
+finalResult <- vector("list", length = numSplit)
+
+
+# main loop
+for (i in 1:numSplit) {
+  for (j in 1:kfold) {
+    cat("numSplit:", i, "/", numSplit, ";", "kfold:", j, "/", kfold, "\n")
+    flush.console()
+    
+    Yfold <- savedFolds[[i]][[j]][[6]]
+
+    ## row is drug, col is target
+    if (db == "en") {
+      theAl <- 0.4
+    } else if (db == "ic") {
+      theAl <- 0.3
+    } else if (db == "gpcr") {
+      theAl <- 0.2
+    } else {
+      theAl <- 0.4
+    }
+    Ypred <- computeRecommendation(A = Yfold, lambda = 0.5, 
+                                   alpha = theAl, S = sd, S1 = st)
+    
+    testSet <- savedFolds[[i]][[j]][[1]]
+    
+    ## result
+    result2 <- evalMetrics(Ypred = Ypred,testSet = testSet)
+    resMetrics[j, ] <- result2
+  }
+  finalResult[[i]] <- resMetrics
+}
+
+# combine result
+resCom <- as.data.frame(data.table::rbindlist(finalResult))
+resMean <- colMeans(resCom)
+se <- sqrt(var(resCom[, 1]) / length(resCom[, 1]))
+cat("DT-Hybrid:", "MPR =", round(resMean, 3), "+\\-", round(se, 3),  "\n")
+flush.console()
+
